b50f05e502904e7f196e6644d1c421d114b2e35f
Renato Gomes <renatoo.agomes@gmail.com>
2018-10-03 06:14:09 -0300

    Listagem OK

--- resources/views/pages/blog/listagem.blade.php
+++ resources/views/pages/blog/listagem.blade.php
@@ -11,16 +11,18 @@
                 <div class="post-card card">
                 
                     <div class="card-header">
-                        <h3 class="mb-3">Baby Blues e Depressão pós-parto não são a mesma coisa?
-                            <p class="data">1 de Junho de 2018</p>
-                        </h3>
+                        <a href="#link-post" class="titulo">
+                            <h3 class="mb-3">Baby Blues e Depressão pós-parto não são a mesma coisa?
+                                <p class="data">1 de Junho de 2018</p>
+                            </h3>
+                        </a>
                     </div>
 
                     <img class="img-responsive" src="https://res.cloudinary.com/tesseract/image/upload/v1535248012/jussara/r94CBE5.png" alt="">
                 
                     <div class="card-body">
                         <p>
-                        A maternidade é comum a mulher se sentir frágil diante das mudanças corporais, hormonais e incertezas do futuro, ela também revela - da concepção ao... <strong>[leia mais]</strong>
+                        A maternidade é comum a mulher se sentir frágil diante das mudanças corporais, hormonais e incertezas do futuro, ela também revela - da concepção ao... <a href="#link-post"><strong>[leia mais]</strong> </a>
                         </p>
                     </div>
 
